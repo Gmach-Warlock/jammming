75ffb5971bee497c9ae3b846478cfc4e2c9f1a88
Gary Yamashita <garyjyam@gmail.com>
2025-05-02 07:37:52 -0700

    pictures moved to public

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import './global.css'
 import HeaderSection from './Components/HeaderSection/HeaderSection'
 import MainSection from './Components/MainSection/MainSection'
 
