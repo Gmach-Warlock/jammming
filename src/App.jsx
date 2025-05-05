9263a2d8cbeef532065d2eabb62c6da102374412
Gary Yamashita <garyjyam@gmail.com>
2025-05-05 16:43:12 -0700

    files moved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Login from './components/Login/Login'
 import Playlist from './components/Playlist/Playlist'
 import SearchBar from './components/SearchBar/SearchBar'
 import SearchResults from './components/SearchResults/SearchResults'
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+      <Login />
       <h1>Jammming</h1>
       <SearchBar />
       <div className="flex-container results-playlist-container">
